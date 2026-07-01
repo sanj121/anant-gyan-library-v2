756c88e6e2cb9360086e1998479b28c2566a76ff
Sanjeet Kumar <112535812+sanj121@users.noreply.github.com>
2026-07-02 04:15:29 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: '/anant-gyan-library-v2/',
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
